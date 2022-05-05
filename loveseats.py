cddf7698894c5fc72823ad51b3b49bf889b57756
jeremy <74825770+jerepe@users.noreply.github.com>
2022-05-05 16:09:21 +0200

    Execution rights for main user added to loveseats.py. Output tested using terminal

---
+++ loveseats.py
@@ -0,0 +1,4 @@
+lovely_loveseat_description = """Lovely Loveseat. Tufted polyester blend on wood.
+32 inches high x 40 inches wide x 30 inches deep.
+Red or white."""
+print(lovely_loveseat_description)
